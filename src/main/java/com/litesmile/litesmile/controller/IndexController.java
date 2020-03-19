12e70d7994bc5e64b077c046a582c1eeb809264c
zhogjiane <zhogjiane@icloud.com>
2020-03-19 11:52:12 +0800

    add page_control

--- src/main/java/com/litesmile/litesmile/controller/IndexController.java
+++ src/main/java/com/litesmile/litesmile/controller/IndexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import com.litesmile.litesmile.dto.PaginationDTO;
 import com.litesmile.litesmile.dto.QuestionDTO;
 import com.litesmile.litesmile.mapper.UserMpper;
 import com.litesmile.litesmile.model.User;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController{
@@ -35,7 +37,9 @@ public class IndexController{
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model                    
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size
     ){
         Cookie[] cookies = request.getCookies();
         if(cookies != null && cookies.length != 0)
@@ -50,8 +54,8 @@ public class IndexController{
             }
         }
         
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 
--- src/main/java/com/litesmile/litesmile/service/QuestionService.java
+++ src/main/java/com/litesmile/litesmile/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.litesmile.litesmile.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.litesmile.litesmile.dto.PaginationDTO;
 import com.litesmile.litesmile.dto.QuestionDTO;
 import com.litesmile.litesmile.mapper.QuestionMapper;
 import com.litesmile.litesmile.mapper.UserMpper;
@@ -27,10 +28,27 @@ public class QuestionService{
 
 
 
-	public List<QuestionDTO> list() {
+	public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
 
-        List<Question> questions = questionMapper.list();
+	   if (page<1)
+	       page = 1;
+
+	   if (page > paginationDTO.getTotalPage())
+	       page = paginationDTO.getTotalPage();
+
+
+
+	    Integer offset = size * (page - 1);
+
+
+
+
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for(Question question: questions){
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -39,7 +57,9 @@ public class QuestionService{
             questionDTOList.add(questionDTO);
 
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
 
 	}
 
